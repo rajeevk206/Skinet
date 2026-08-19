ce2f2e3c2938d808258ae242e2567f2b59fcf7a1
rajeevk206 <rajeevk206@gmail.com>
2026-08-19 23:52:00 +0530

    remove old url

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -3,6 +3,5 @@ export const environment = {
 
      apiUrl: 'https://skinet-api-bscbeef6cubvbbe0.indiasouthcentral-01.azurewebsites.net/api/',
      hubUrl: 'https://skinet-api-bscbeef6cubvbbe0.indiasouthcentral-01.azurewebsites.net/hub/notifications',
-    
-  stripePublicKey: 'pk_test_51U2jMt5Y5LYd9WBKf6ZikrZwo58DZulRUJrvM7YVYk3UkCJ7GyswLOb0OE2Ws7pxhycNG4cMc8uD3zV8DNxU88BL00qYOs8Yx2'
+     stripePublicKey: 'pk_test_51U2jMt5Y5LYd9WBKf6ZikrZwo58DZulRUJrvM7YVYk3UkCJ7GyswLOb0OE2Ws7pxhycNG4cMc8uD3zV8DNxU88BL00qYOs8Yx2'
 };
