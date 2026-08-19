67c6df82234cdbdf60e63acc91ecd2cbd23493bc
rajeevk206 <rajeevk206@gmail.com>
2026-08-20 01:18:01 +0530

    envirement file change

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,8 +1,8 @@
-export const environment = {
-  production: false,
+// export const environment = {
+//   production: false,
 
-  apiUrl: 'https://localhost:7001/api/',
-  hubUrl: 'https://localhost:7001/hub/notifications',
+//   apiUrl: 'https://localhost:7001/api/',
+//   hubUrl: 'https://localhost:7001/hub/notifications',
 
-  stripePublicKey: 'pk_test_51U1OuZ9S7b6DDmhGQjj2fYhQfX3iuHdBU19GYncssBScQJGLF8k9GvHfoo4GFBFqoL2zWt2P6OXmeWJ1QzqLiA5z00IucBe0wX'
-};
+//   stripePublicKey: 'pk_test_51U1OuZ9S7b6DDmhGQjj2fYhQfX3iuHdBU19GYncssBScQJGLF8k9GvHfoo4GFBFqoL2zWt2P6OXmeWJ1QzqLiA5z00IucBe0wX'
+// };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,7 +1,17 @@
-export const environment = {
-  production: true,
+// export const environment = {
+//   production: true,
 
-     apiUrl: 'https://skinet-api-bscbeef6cubvbbe0.indiasouthcentral-01.azurewebsites.net/api/',
-     hubUrl: 'https://skinet-api-bscbeef6cubvbbe0.indiasouthcentral-01.azurewebsites.net/hub/notifications',
-     stripePublicKey: 'pk_test_51U2jMt5Y5LYd9WBKf6ZikrZwo58DZulRUJrvM7YVYk3UkCJ7GyswLOb0OE2Ws7pxhycNG4cMc8uD3zV8DNxU88BL00qYOs8Yx2'
+//      apiUrl: 'https://skinet-api-bscbeef6cubvbbe0.indiasouthcentral-01.azurewebsites.net/api/',
+//      hubUrl: 'https://skinet-api-bscbeef6cubvbbe0.indiasouthcentral-01.azurewebsites.net/hub/notifications',
+//      stripePublicKey: 'pk_test_51U2jMt5Y5LYd9WBKf6ZikrZwo58DZulRUJrvM7YVYk3UkCJ7GyswLOb0OE2Ws7pxhycNG4cMc8uD3zV8DNxU88BL00qYOs8Yx2'
+// };
+
+
+export const environment = {
+  production: false,
+
+  apiUrl: 'https://localhost:7001/api/',
+  hubUrl: 'https://localhost:7001/hub/notifications',
+
+  stripePublicKey: 'pk_test_51U1OuZ9S7b6DDmhGQjj2fYhQfX3iuHdBU19GYncssBScQJGLF8k9GvHfoo4GFBFqoL2zWt2P6OXmeWJ1QzqLiA5z00IucBe0wX'
 };
