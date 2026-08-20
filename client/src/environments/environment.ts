9ee68ce2b5ccf9ce2b7650d37c79d77f0b120f00
rajeevk206 <rajeevk206@gmail.com>
2026-08-20 12:03:11 +0530

    huburl

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
 
     // apiUrl: 'apiUrl',
-     hubUrl: 'hubUrl',
+     //hubUrl: 'hubUrl',
      apiUrl: 'https://skinet-api-bscbeef6cubvbbe0.indiasouthcentral-01.azurewebsites.net/api/',
-     //hubUrl: 'https://skinet-api-bscbeef6cubvbbe0.indiasouthcentral-01.azurewebsites.net/hub/notifications',
+     hubUrl: 'https://skinet-api-bscbeef6cubvbbe0.indiasouthcentral-01.azurewebsites.net/hub/notifications',
      stripePublicKey: 'pk_test_51U2jMt5Y5LYd9WBKf6ZikrZwo58DZulRUJrvM7YVYk3UkCJ7GyswLOb0OE2Ws7pxhycNG4cMc8uD3zV8DNxU88BL00qYOs8Yx2'
 };
